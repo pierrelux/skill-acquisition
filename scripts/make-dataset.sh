bdcabead04fc31d6e858ce0a501e2a9b10a927df
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-02-10 02:06:05 -0500

    Add script to select trajectories and build dataset

---
+++ scripts/make-dataset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $# < 2 ]]; then
+ echo "$0 [MAXSTEPS] [EPISODES] "
+ echo "Create dataset from qualifing trajectories"
+ exit -1
+fi	
+
+# Select terminated trajectories under a given number of steps
+cat `awk '$1 < MAXSTEPS && $3==1 { print "pinball_observations" FNR ".dat" }' MAXSTEPS=$1 $2`
