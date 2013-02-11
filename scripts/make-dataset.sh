62c1aab6ffb01219858b0ef7a860713fe567584a
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-02-10 23:49:29 -0500

    Remove duplicate observations when building dataset

--- scripts/make-dataset.sh
+++ scripts/make-dataset.sh
@@ -4,7 +4,7 @@ if [[ $# < 2 ]]; then
  echo "$0 [MAXSTEPS] [EPISODES] "
  echo "Create dataset from qualifing trajectories"
  exit -1
-fi	
+fi
 
 # Select terminated trajectories under a given number of steps
-cat `awk '$1 < MAXSTEPS && $3==1 { print "pinball_observations" FNR ".dat" }' MAXSTEPS=$1 $2`
+cat `awk '$1 < MAXSTEPS && $3==1 { print "pinball_observations" FNR ".dat" }' MAXSTEPS=$1 $2` | sort | uniq -u
