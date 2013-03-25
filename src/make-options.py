e6ad2f7885be2ef877e3fb7ece5678e103edcbd6
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-03-25 12:39:13 -0400

    Fix runtime errors. Can now learn set of options

--- src/make-options.py
+++ src/make-options.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
+import os
+import csv
 import copy
 import time
 import pickle
+import random
 import argparse
 import numpy as np
 
@@ -93,14 +96,14 @@ class PseudoRewardEnvironment(Environment):
 	:rtype: bool
 
 	"""
-        knn_idx, dist = self.kdtree.nn_index(observation)
+        knn_idx, dist = self.kdtree.nn_index(np.array([observation]))
         return self.membership[knn_idx] == self.target_community
 
     def env_step(self, action):
 	returnRO = self.decorated.env_step(action)
 	returnRO.terminal = self.episode_ended(returnRO.o.doubleArray)
 	if returnRO.terminal:
-	    returnRO.r = END_EPISODE
+	    returnRO.r = self.END_EPISODE
 
 	return returnRO
 
@@ -113,13 +116,13 @@ class PseudoRewardEnvironment(Environment):
     def env_cleanup(self):
 	self.decorated.env_cleanup()
 
-    def env_message(message):
-	return self.decorated.env_message()
+    def env_message(self, message):
+	return self.decorated.env_message(message)
 
 def timestamp():
     return time.strftime('%Y-%m-%d-%H-%M-%S.pl')
 
-def learn_options(dataset, index, cl, num_episodes):
+def learn_options(dataset, index, cl, num_episodes, prefix):
     """ From the graph clustering step, learn options to navigate between adjacent communities.
 
     :param dataset: The points used build the graph
@@ -143,46 +146,60 @@ def learn_options(dataset, index, cl, num_episodes):
 
     # Learn options
     for option in options:
+	print 'Learning option from %d to %d'%(option.source_community, option.target_community)
+
 	agent = sarsa_lambda(epsilon=0.01, alpha=0.001, gamma=1.0, lmbda=0.9, params={'name':'fourier', 'order':4})
-	environment = PseudoRewardEnvironment(PinballRLGlue('pinball_simple_single.cfg'),
+	environment = PseudoRewardEnvironment(PinballRLGlue('pinball_hard_single.cfg'),
 			option.target_community, cl.membership, index)
 
         # Connect to RL-Glue
-        RLGlue = RLGlueLocal.LocalGlue(environment, agent)
+        rlglue = RLGlueLocal.LocalGlue(environment, agent)
+	rlglue.RL_init()
 
         # Execute episodes
-	option_file_prefix = 'option-from-%d-to-%d-%s'%(option.source_community, option.target_community, timestamp())
-        score_file = csv.writer(open(option_file_prefix + '-score.csv', 'wb'))
+        score_file = csv.writer(open(prefix + '-score.csv', 'wb'))
 
 	for i in xrange(num_episodes):
 	    # Use all of the nodes in the source community as possible
             # initial positions. Selected at random in each episode
-	    start_position = dataset[random.choice(cl[option.source])][:2]
-            RLGlue.RL_env_message('set-start-state ' + start_position[0] + " " + start_position[1])
+	    start_position = dataset[random.choice(cl[option.source_community])][:2]
+	    rlglue.RL_env_message('set-start-state %f %f'%(start_position[0], start_position[1]))
 
-            terminated = RLGlue.RL_episode(MAX_STEPS)
+	    print '\tEpsiode %d of %d starting at %f, %f'%(i, num_episodes, start_position[0], start_position[1])
+            terminated = rlglue.RL_episode(MAX_STEPS)
 
-	    total_steps = RLGlue.RL_num_steps()
-            total_reward = RLGlue.RL_return()
+	    total_steps = rlglue.RL_num_steps()
+            total_reward = rlglue.RL_return()
+	    print '\t\t %d steps, %d reward, %d terminated'%(total_steps, total_reward, terminated)
 
             score_file.writerow([i, total_steps, total_reward, terminated])
 
-        score_file.close()
-
 	option.weights = agent.weights.copy()
 	option.basis = copy.deepcopy(agent.basis)
+	rlglue.RL_cleanup()
 
     # Serialize options
-    pickle(options, open(option_file_prefix + '.pl', 'wb'))
+    pickle.dump(options, open(prefix + '-options.pl', 'wb'))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create options from the community structures')
     parser.add_argument('dataset', help='Input dataset used to build the index')
     parser.add_argument('index', help='KNN index')
     parser.add_argument('clustering', help='clustering obtained from the community detection algorithm')
-    parser.add_argument('-e', '--number-episodes', dest='nepisodes', type=int, default=100, help='the number of episodes to execute for learning each option (default: 100)')
-
+    parser.add_argument('-n', '--number-episodes', dest='nepisodes', type=int, default=100, help='the number of episodes to execute for learning each option (default: 100)')
+    parser.add_argument('-p', '--prefix', action='store', type=str, dest='prefix', help="output prefix (default: dataset)")
     args = parser.parse_args()
+    if not args.prefix:
+        args.prefix = os.path.splitext(os.path.basename(args.dataset))[0]
+
+    # Import dataset
+    print 'Loading dataset...'
+    dataset = np.loadtxt(args.dataset)
+
+    # Import index
+    print 'Loading index...'
+    flann = FLANN()
+    flann.load_index(args.index, dataset)
 
     # Open the vertex dendogram
     print 'Loading clustering...'
@@ -194,14 +211,5 @@ if __name__ == "__main__":
     print cl.graph.vcount(), ' vertices'
     print len(cl), ' communities'
 
-    # Import dataset
-    print 'Loading dataset...'
-    dataset = np.loadtxt(args.dataset)
-
-    # Import index
-    print 'Loading index...'
-    flann = FLANN()
-    flann.load_index(args.index, dataset)
-
     # Create and learn options
-    #learn_options(dataset, flann, cl, args.nepisodes)
+    learn_options(dataset, flann, cl, args.nepisodes, args.prefix)
