88ba2edc5cf72adea07941fc66b29a66611fbfe5
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-05-14 17:37:06 -0400

    Add local scaling of the heat kernel according to Zelnik-Manor

--- src/build-graph.py
+++ src/build-graph.py
@@ -21,7 +21,10 @@ group.add_argument('--mutual', metavar='KNN', type=int, help="build a mutual knn
 group.add_argument('--symmetric', metavar='KNN', type=int, help="build a symmetric knn neighborhood graph")
 group.add_argument('--radius', metavar='RADIUS', type=float, help="build a r-neighborhood graph")
 
-parser.add_argument('--sigma', action='store', type=float, default=0.025, dest='sigma', help='sigma value (default: 0.5)')
+group = parser.add_mutually_exclusive_group(required=False)
+group.add_argument('--sigma', type=float, default=None, help='sigma value (default: 0.5)')
+group.add_argument('--local-scaling', metavar='JTH', type=int, default=None, help="use local scaling according to Zelnik-Manor and Perona")
+
 parser.add_argument('--delete', action='store_true', help="delete singleton vertices")
 parser.add_argument('--prefix', action='store', type=str, dest='prefix', help="output prefix (default: dataset)")
 
@@ -42,8 +45,6 @@ flann.build_index(dataset)
 graph = ig.Graph(directed=args.directed)
 graph.add_vertices(np.alen(dataset))
 
-similarity = lambda dist: np.exp(-1*dist/args.sigma)
-
 if args.radius:
     print 'Searching all nearest neighbors within r=%f...'%(args.radius,)
     edges = []
@@ -51,7 +52,7 @@ if args.radius:
     for i in xrange(np.alen(dataset)):
         nn, dists = flann.nn_radius(dataset[i,:], args.radius)
         edges.extend(((i, j) for j in nn))
-        weights.extend((similarity(d) for d in dists))
+        weights.extend((np.exp(-1*d/args.sigma) for d in dists))
 
     graph.add_edges(edges)
     graph.es["weight"] = weights
@@ -59,23 +60,35 @@ else:
     k = next((n for n in [args.directed, args.mutual, args.symmetric] if n))
 
     print 'Searching all %d nearest neighbors in kd-tree...'%(k,)
-    knn, dists = flann.nn_index(dataset, k)
+    knn, dists = flann.nn_index(dataset, k+1)
     knn = knn[:, 1:]
     dists = dists[:, 1:]
 
+    def heat_kernel(i, j):
+        return np.exp(-1*dists[i,j]/args.sigma)
+
+    def locally_scaled_heat_kernel(i, j, l):
+        return np.exp(-1*dists[i,j]/(np.sqrt(dists[i,l])*np.sqrt(dists[knn[i,j],l])))
+
+    if args.local_scaling:
+        print 'Scaling locally'
+        similarity = lambda i, j: locally_scaled_heat_kernel(i, j, args.local_scaling-1)
+    else:
+        similarity = heat_kernel
+
     if args.directed:
         print 'Building directed graph...'
-        edges, weights = zip(*(((i, knn[i, j]), similarity(dists[i, j])) for i in xrange(knn.shape[0])
+        edges, weights = zip(*(((i, knn[i, j]), similarity(i, j)) for i in xrange(knn.shape[0])
             for j in xrange(knn.shape[1])))
 
     if args.mutual:
         print 'Building mutual graph...'
-        edges, weights = zip(*(((i, knn[i, j]), similarity(dists[i, j])) for i in xrange(knn.shape[0])
+        edges, weights = zip(*(((i, knn[i, j]), similarity(i, j)) for i in xrange(knn.shape[0])
             for j in xrange(knn.shape[1]) if i in knn[knn[i, j],:]))
 
     if args.symmetric:
         print 'Building symmetric graph...'
-        uniq_edges = dict(((frozenset((i, knn[i, j])), similarity(dists[i, j])) for i in xrange(knn.shape[0])
+        uniq_edges = dict(((frozenset((i, knn[i, j])), similarity(i, j)) for i in xrange(knn.shape[0])
             for j in xrange(knn.shape[1])))
 
         edges = map(list, uniq_edges.iterkeys())
