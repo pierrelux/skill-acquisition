8d3358599cd03a646eaa1a9806bb61989d233175
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-05-15 17:03:01 -0400

    Add weighting option

--- src/find-communities.py
+++ src/find-communities.py
@@ -9,18 +9,33 @@ parser = argparse.ArgumentParser(description='Find the community structures')
 parser.add_argument('graph', help='the state-space graph')
 parser.add_argument('-t', '--time', action='store', type=int, default=4, dest='time', help='length t of the random walks (default: 4)')
 parser.add_argument('-p', '--prefix', action='store', type=str, dest='prefix', help="output prefix (default: graph)")
+parser.add_argument('--weighted', action='store_true', help="Use weighted graph")
+parser.add_argument('-v', '--verbose', action='store_true', help="Verbose mode")
+
 args = parser.parse_args()
 if not args.prefix:
     args.prefix = os.path.splitext(os.path.basename(args.graph))[0]
 
 # Load graph
-print 'Loading graph...'
+if args.verbose:
+    print 'Loading graph...'
+
 graph = ig.load(args.graph)
 
 # Compute community structure
-print 'Finding communities...'
-vd = graph.community_walktrap(weights='weight', steps=args.time)
+if args.verbose:
+    print 'Finding communities...'
+
+if args.weighted:
+  vd = graph.community_walktrap(weights='weight', steps=args.time)
+else:
+  vd = graph.community_walktrap(steps=args.time)
+
+cl = vd.as_clustering()
+print '%d %d %f'%(args.time, len(cl), cl.modularity)
 
 # Save clustering
-print 'Saving clustering...'
+if args.verbose:
+    print 'Saving clustering...'
+
 pickle.dump(vd, open(args.prefix + '-clustering.pickle', 'wb'))
