ff158759a7a6963f3746d3da5af95c40339f78c0
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-05-28 14:33:06 -0400

    Decouples option implementation with option training

--- src/learn-behavior-policy.py
+++ src/learn-behavior-policy.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import os
 import csv
 import cPickle
@@ -9,70 +8,50 @@ from pyflann import *
 from pyrl.rlglue import RLGlueLocal as RLGlueLocal
 from pyrl.environments.pinball import PinballRLGlue
 
-def learn_policy(options, index, nepisodes, max_steps, prefix):
-    agent = IntraOptionLearning(options)
-    environment = PinballRLGlue('pinball_hard_single.cfg')
+def learn_policy(options, environment_name, nepisodes, max_steps, prefix):
+    agent = IntraOptionLearning(options, alpha=0.01, gamma=1.0, epsilon=0.1, fa_order=4)
+    environment = PinballRLGlue(environment_name)
 
     score_file = csv.writer(open(prefix + '-behavior-policy.csv', 'wb'))
+
     # Connect to RL-Glue
-    rlglue = RLGlueLocal.LocalGlue(environment, agent)
+    rlglue = RLGlueLocal.LocalGlue(TrajectoryRecorder(environment, 'option-trajectory.dat'), agent)
     rlglue.RL_init()
 
     for i in xrange(nepisodes):
-	print 'Episode ', i
-	terminated = rlglue.RL_episode(max_steps)
-	total_steps = rlglue.RL_num_steps()
-	total_reward = rlglue.RL_return()
+        print 'Episode ', i
+        terminated = rlglue.RL_episode(max_steps)
+        total_steps = rlglue.RL_num_steps()
+        total_reward = rlglue.RL_return()
 
-	print '\t %d steps, %d reward, %d terminated'%(total_steps, total_reward, terminated)
-	score_file.writerow([i, total_steps, total_reward, terminated])
+        print '\t %d steps, %d reward, %d terminated'%(total_steps, total_reward, terminated)
+        score_file.writerow([i, total_steps, total_reward, terminated])
 
-	rlglue.RL_cleanup()
+    rlglue.RL_cleanup()
 
-    # TODO define proper serialization
     cPickle.dump(agent, open(prefix + '-behavior-policy.pl', 'wb'))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Learn the behavior policy over options')
-    parser.add_argument('dataset', help='input dataset used to build the index')
-    parser.add_argument('index', help='an k-nearest neighbor index')
-    parser.add_argument('clustering', help='clustering obtained from the\
-		    community detection algorithm')
     parser.add_argument('options', help='a set of options for this task')
+    parser.add_argument('environment', help='environment configuration')
     parser.add_argument('-n', '--number-episodes', dest='nepisodes', type=int,
-		    default=100, help='the number of episodes to execute for\
-		    learning the policy over options (default: 100)')
+                    default=100, help='the number of episodes to execute for\
+                    learning the policy over options (default: 100)')
     parser.add_argument('-s', '--max-steps', dest='max_steps', type=int,
-		    default=10000, help='the maximum number of steps that the\
-		    agent is allowed to take in the environment')
+                    default=10000, help='the maximum number of steps that the\
+                    agent is allowed to take in the environment')
     parser.add_argument('-p', '--prefix', action='store', type=str,
-		    dest='prefix', help="output prefix (default: dataset)")
+                    dest='prefix', help="output prefix (default: dataset)")
     args = parser.parse_args()
 
     if not args.prefix:
-        args.prefix = os.path.splitext(os.path.basename(args.dataset))[0]
-
-    # Import dataset
-    print 'Loading dataset...'
-    dataset = np.loadtxt(args.dataset)
-
-    # Import index
-    print 'Loading index...'
-    flann = FLANN()
-    flann.load_index(args.index, dataset)
-
-    # Open the vertex dendogram
-    print 'Loading clustering...'
-    vd = cPickle.load(open(args.clustering, 'rb'))
-    cl = vd.as_clustering()
+        args.prefix = os.path.splitext(os.path.basename(args.options))[0]
 
     # Import options
     print 'Loading options...'
     options = cPickle.load(open(args.options, 'rb'))
-    for option in options:
-	option.index = flann
-	option.membership = cl.membership
+    print 'Learning with %d options'%(len(options),)
 
     # Learn the behavior policy
-    learn_policy(options, flann, args.nepisodes, args.max_steps, args.prefix)
-
+    learn_policy(options, args.environment, args.nepisodes, args.max_steps, args.prefix)
