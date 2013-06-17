4c1808865243f52e39e3b47b2a799f763cead031
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-06-17 17:08:14 -0400

    Add graph completion when proximity graph is disconnected

--- src/build-graph.py
+++ src/build-graph.py
@@ -25,7 +25,13 @@ group = parser.add_mutually_exclusive_group(required=False)
 group.add_argument('--sigma', type=float, default=None, help='sigma value (default: 0.5)')
 group.add_argument('--local-scaling', metavar='JTH', type=int, default=None, help="use local scaling according to Zelnik-Manor and Perona")
 
+group = parser.add_mutually_exclusive_group(required=False)
+group.add_argument('--edgelist', action='store_true', help="export graph under edgelist format")
+group.add_argument('--pickle', action='store_true', help="export graph under edgelist format")
+group.add_argument('--adjacency', action='store_true', help="export graph under edgelist format")
+
 parser.add_argument('--delete', action='store_true', help="delete singleton vertices")
+parser.add_argument('--complete', action='store_true', help="complete singleton vertices to their 1st nn")
 parser.add_argument('--prefix', action='store', type=str, dest='prefix', help="output prefix (default: dataset)")
 
 args = parser.parse_args()
@@ -44,6 +50,18 @@ flann.build_index(dataset)
 # Create the state-space graph
 graph = ig.Graph(directed=args.directed)
 graph.add_vertices(np.alen(dataset))
+
+def heat_kernel(i, j, dists, knn=None):
+    return np.exp(-1*dists[i,j]/args.sigma)
+
+def locally_scaled_heat_kernel(i, j, dists, knn):
+    return np.exp(-1*dists[i,j]/(np.sqrt(dists[i,args.local_scaling-1])*np.sqrt(dists[knn[i,j],args.local_scaling-1])))
+
+if args.local_scaling:
+    print 'Scaling locally'
+    similarity = locally_scaled_heat_kernel
+else:
+    similarity = heat_kernel
 
 if args.radius:
     print 'Searching all nearest neighbors within r=%f...'%(args.radius,)
@@ -64,31 +82,19 @@ else:
     knn = knn[:, 1:]
     dists = dists[:, 1:]
 
-    def heat_kernel(i, j):
-        return np.exp(-1*dists[i,j]/args.sigma)
-
-    def locally_scaled_heat_kernel(i, j, l):
-        return np.exp(-1*dists[i,j]/(np.sqrt(dists[i,l])*np.sqrt(dists[knn[i,j],l])))
-
-    if args.local_scaling:
-        print 'Scaling locally'
-        similarity = lambda i, j: locally_scaled_heat_kernel(i, j, args.local_scaling-1)
-    else:
-        similarity = heat_kernel
-
     if args.directed:
         print 'Building directed graph...'
-        edges, weights = zip(*(((i, knn[i, j]), similarity(i, j)) for i in xrange(knn.shape[0])
+        edges, weights = zip(*(((i, knn[i, j]), similarity(i, j, dists, knn)) for i in xrange(knn.shape[0])
             for j in xrange(knn.shape[1])))
 
     if args.mutual:
         print 'Building mutual graph...'
-        edges, weights = zip(*(((i, knn[i, j]), similarity(i, j)) for i in xrange(knn.shape[0])
+        edges, weights = zip(*(((i, knn[i, j]), similarity(i, j, dists, knn)) for i in xrange(knn.shape[0])
             for j in xrange(knn.shape[1]) if i in knn[knn[i, j],:]))
 
     if args.symmetric:
         print 'Building symmetric graph...'
-        uniq_edges = dict(((frozenset((i, knn[i, j])), similarity(i, j)) for i in xrange(knn.shape[0])
+        uniq_edges = dict(((frozenset((i, knn[i, j])), similarity(i, j, dists, knn)) for i in xrange(knn.shape[0])
             for j in xrange(knn.shape[1])))
 
         edges = map(list, uniq_edges.iterkeys())
@@ -107,11 +113,11 @@ else:
 if len(components) > 1:
     print '\033[31mGraph is disconnected\033[00m'
 
-    if args.delete:
-        spurious_vertices = []
-        for component in islice(components, 1, None):
-            spurious_vertices.extend(component.vs['index'])
+    spurious_vertices = []
+    for component in islice(components, 1, None):
+        spurious_vertices.extend(component.vs['index'])
 
+    if args.delete:
         # Only keep the largest component
         graph = components[0]
 
@@ -128,11 +134,33 @@ if len(components) > 1:
         args.prefix = args.prefix + '-pruned'
         np.savetxt(args.prefix + '.dat', dataset)
 
-else:
+    elif args.complete:
+        print 'Completing singleton vertices to their %d nn...'%(k,)
+        uniq_edges = dict(((frozenset((i, knn[i, j])), similarity(i, j, dists, knn))
+                         for i in spurious_vertices
+                             for j in xrange(knn.shape[1])))
+
+        edges = map(list, uniq_edges.iterkeys())
+
+        weights = graph.es['weight']
+        weights.extend(uniq_edges.values())
+
+        graph.add_edges(edges)
+        graph.es['weight'] = weights
+
+if graph.is_connected():
     print '\033[92mGraph is connected \033[00m'
 
 print 'Saving graph...'
-graph.save(args.prefix + '-graph.pickle', format="pickle")
+if args.edgelist:
+    graph_format = 'edgelist'
+    graph_extension = '.edges'
+else:
+    graph_format = 'pickle'
+    graph_extension = '.pickle'
+
+graph.save(args.prefix + '-graph' + graph_extension, format=graph_format)
 
 print 'Saving kd-tree...'
 flann.save_index(args.prefix + '-index.knn')
+
