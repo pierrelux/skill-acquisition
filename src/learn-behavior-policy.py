5659803cecefd85ac582eccdd7e22461ae117e15
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-06-17 20:44:19 -0400

    Parallelize behavior policy learning. Sends text when finished.

--- src/learn-behavior-policy.py
+++ src/learn-behavior-policy.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python
 import os
-import csv
+import pp
+import time
 import cPickle
+import smtplib
 import argparse
-from options import *
-from pyflann import *
-from pyrl.rlglue import RLGlueLocal as RLGlueLocal
-from pyrl.environments.pinball import PinballRLGlue
 
-def learn_policy(options, environment_name, nepisodes, max_steps, prefix):
-    agent = IntraOptionLearning(options, alpha=0.01, gamma=1.0, epsilon=0.1, fa_order=4)
+def learn_policy(options_learned, environment_name, nepisodes, max_steps, prefix):
+    import csv
+    import cPickle
+    import options
+    from pyrl.rlglue import RLGlueLocal as RLGlueLocal
+    from pyrl.environments.pinball import PinballRLGlue
+
+    agent = options.IntraOptionLearning(options_learned, alpha=0.01, gamma=1.0, epsilon=0.1, fa_order=4)
     environment = PinballRLGlue(environment_name)
 
     score_file = csv.writer(open(prefix + '-behavior-policy.csv', 'wb'))
 
     # Connect to RL-Glue
-    rlglue = RLGlueLocal.LocalGlue(TrajectoryRecorder(environment, 'option-trajectory.dat'), agent)
+    rlglue = RLGlueLocal.LocalGlue(environment, agent)
     rlglue.RL_init()
 
+    scores = []
     for i in xrange(nepisodes):
         print 'Episode ', i
         terminated = rlglue.RL_episode(max_steps)
@@ -25,11 +30,21 @@ def learn_policy(options, environment_name, nepisodes, max_steps, prefix):
         total_reward = rlglue.RL_return()
 
         print '\t %d steps, %d reward, %d terminated'%(total_steps, total_reward, terminated)
-        score_file.writerow([i, total_steps, total_reward, terminated])
+        score = [i, total_steps, total_reward, terminated]
+        scores.append(score)
+        score_file.writerow(score)
 
     rlglue.RL_cleanup()
 
     cPickle.dump(agent, open(prefix + '-behavior-policy.pl', 'wb'))
+
+    return scores
+
+def job_done(msg):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('pierrelucbacon@gmail.com', 'password')
+    server.sendmail('pierrelucbacon@gmail.com', 'phone@msg.telus.com', msg)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Learn the behavior policy over options')
@@ -41,6 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--max-steps', dest='max_steps', type=int,
                     default=10000, help='the maximum number of steps that the\
                     agent is allowed to take in the environment')
+    parser.add_argument('-a', '--number-agents', type=int, default=100, help='number of agents to average over')
     parser.add_argument('-p', '--prefix', action='store', type=str,
                     dest='prefix', help="output prefix (default: dataset)")
     args = parser.parse_args()
@@ -50,8 +66,21 @@ if __name__ == "__main__":
 
     # Import options
     print 'Loading options...'
-    options = cPickle.load(open(args.options, 'rb'))
-    print 'Learning with %d options'%(len(options),)
+    start_time = time.time()
+    options_learned = cPickle.load(open(args.options, 'rb'))
+    print 'Learning with %d options'%(len(options_learned),)
+
+    job_server = pp.Server(ppservers=("*",))
 
     # Learn the behavior policy
-    learn_policy(options, args.environment, args.nepisodes, args.max_steps, args.prefix)
+    jobs = [job_server.submit(learn_policy,
+               (options_learned, args.environment, args.nepisodes, args.max_steps, args.prefix))
+                   for agents in xrange(args.number_agents)]
+
+    job_server.wait()
+    job_server.print_stats()
+
+    scores = [job() for job in jobs]
+    cPickle.dump(scores, open(args.prefix + '-behavior-policy-aggregated.pl', 'wb'))
+
+    job_done('Experiment has finished')
